81c6c486b4761fc3ac1bdc7ba954c069a05921ee
Gustavo Ralek <gpenaloza@despegar.com>
2018-07-31 15:19:51 -0300

    Agrego Sockets

---
+++ public/js/tok.js
@@ -0,0 +1,41 @@
+var socket = io();
+
+$('#passe1').modal('show');
+
+const enigma = {
+    1: {id: "#e1", result: "soberano"},
+    2: {id: "#e2", result: "pepe"}
+};
+
+
+for (const prop in enigma) {
+
+
+    socket.on('message', function (data) {
+        console.log("reading... ", data);
+
+        $(data.id).val(data.value);
+    });
+
+    let element = $(enigma[prop].id);
+    element.on('input', function () {
+
+        let message = {"id": enigma[prop].id, "value": element.val()}
+
+        console.log('emit...', message);
+        socket.emit('message', message);
+        return false;
+    });
+
+}
+
+
+let validateEnigma = (id) => {
+
+    if (enigma[id].element.val().toLowerCase() !== enigma[id].result) {
+        enigma[id].element.addClass("is-invalid");
+    }
+
+};
+
+
